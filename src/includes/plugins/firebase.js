487b359b8784bd8159d07d6756ae4717482631c1
Koushik Rajbanshi <71749251+koushik172@users.noreply.github.com>
2023-05-04 19:40:17 +0530

    Update firebase.js

--- src/includes/plugins/firebase.js
+++ src/includes/plugins/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, createUserWithEmailAndPassword, updateProfile, signInWithEmail
 import { getFirestore, collection, addDoc,  setDoc, doc } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAa3bbD-EQDc1qeu46SaV-AM1rlvshUAYg',
+  apiKey: 'Your firebase apikey',
   authDomain: 'musicapp-7535c.firebaseapp.com',
   projectId: 'musicapp-7535c',
   storageBucket: 'musicapp-7535c.appspot.com',
-  messagingSenderId: '595461389254',
-  appId: '1:595461389254:web:b4070a767164c1906e50c7'
+  messagingSenderId: 'messegeSenderId',
+  appId: 'Your firebase appId'
 }
 
 firebase.initializeApp(firebaseConfig)
